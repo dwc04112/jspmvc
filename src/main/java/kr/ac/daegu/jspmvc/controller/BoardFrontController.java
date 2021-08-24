1b464a8077fa918d9e2a63ed684488def67cf26a
dwc04112 <dwc04112@gmail.com>
2021-08-24 17:40:31 +0900

    no message

--- src/main/java/kr/ac/daegu/jspmvc/controller/BoardFrontController.java
+++ src/main/java/kr/ac/daegu/jspmvc/controller/BoardFrontController.java
@@ -9,6 +9,8 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @WebServlet("*.bbs")
+//모든 문자열.bbs 로 들어오는 url요청은 여기서 처리해라
+
 public class BoardFrontController extends HttpServlet {
 
     @Override
@@ -19,7 +21,7 @@ public class BoardFrontController extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.setCharacterEncoding("UTF-8");
-        String requestURI = request.getRequestURI();
+        String requestURI = request.getRequestURI();    //localhost:8080으로 호출을 했을 때
         String contextPath = request.getContextPath();
         String cmdURI = requestURI.substring(contextPath.length());
         // log
@@ -32,7 +34,6 @@ public class BoardFrontController extends HttpServlet {
         if(cmdURI.equals("/boardList.bbs")){
             viewPage = "view/boardList.jsp";
         }
-
         RequestDispatcher dispatcher = request.getRequestDispatcher(viewPage);
         dispatcher.forward(request, response);
 
